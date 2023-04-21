24992b50aa322972d38e0044d39dbb330497d77c
Catherine Mei <meic1212@mit.edu>
2023-04-21 15:39:46 -0400

    new js files

--- catherine_testing.py
+++ catherine_testing.py
@@ -2,8 +2,7 @@ import librosa
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import wave
-import struct
+import wavio
 
 
 def plot_beats(sound_y, beats):
@@ -39,39 +38,40 @@ def plot_fundamental_freqs(sound_y, f0s):
     plt.show()
 
 
-def process_fundamental_freqs(f0s, f0_times):
+def process_fundamental_freqs(f0_freq, f0_time):
     """
     Given the fundamental frequencies and corresponding times, remove all nans from list
-    :param f0s: list of fundamental frequencies
-    :param f0_times: list of times corresponding to the fundamental frequencies
+    :param f0_freq: list of fundamental frequencies
+    :param f0_time: list of times corresponding to the fundamental frequencies
     :return: f0s and f0_times with nan values filtered out; raw note information list
     Writes js file with JSON object of the notes
     """
     new_f0s = []
     new_times = []
 
-    for i in range(len(f0s)):
-        if str(f0s[i]) != 'nan':
-            new_f0s.append(f0s[i])
-            new_times.append(f0_times[i])
+    for i in range(len(f0_freq)):
+        if str(f0_freq[i]) != 'nan':
+            new_f0s.append(f0_freq[i])
+            new_times.append(f0_time[i])
 
     return new_f0s, new_times
 
 
-def segment_notes(note_onsets, f0s_freq, f0_time):
+def segment_notes(note_onsets, f0s_freq, f0_time, file_name):
     """
     Given a list of onsets and all of the fundamental frequencies and timestamps of those frequencies,
     generate a CSV with the start and end timestamps of a note as well as the note frequency and letter
     :param note_onsets: estimated beat event locations in time (seconds)
     :param f0s_freq: fundamental frequencies
     :param f0_time: times that the fundamental frequences occur
+    :param file_name: file name of output json for notes
     :return: pandas Dataframe and raw note_info list
     """
 
     # (timestamp start, timestamp end, note frequency, note letter, frequencies included)
     note_info = []
     freq_pt = 0
-    json_obj_str = "export let notes = ["
+    json_obj_str = "var notes = ["
 
     for t in range(len(note_onsets) - 1):
         window_start = note_onsets[t]
@@ -117,12 +117,12 @@ def segment_notes(note_onsets, f0s_freq, f0_time):
             note_freq = librosa.note_to_hz(note)
             note_info.append((window_times[0], window_times[-1], note_freq, note, window_freqs))
             note_obj = "{ start_sec: " + str(window_times[0]) + ", end_sec: " + str(
-                window_times[-1]) + ", note_freq_hz: " + str(note_freq) + ", note_name: '" + note + "' }]"
+                window_times[-1]) + ", note_freq_hz: " + str(note_freq) + ", note_name: '" + note + "' }];"
             json_obj_str += note_obj
         else:
-            json_obj_str += "]"
+            json_obj_str += "];"
 
-    f = open("notes.js", "w")
+    f = open(f'js_files_notes/{file_name}', "w")
     f.write(json_obj_str)
     f.close()
 
@@ -152,49 +152,49 @@ def combine_onset_times(onset_detect, beat_track):
 
 
 def generate_wav(notes_list, sample_rate, file_name):
-    wave_file = wave.open(file_name, 'w')
+    """
+    Given the notes, sample rate, and output filename, generate WAV file of processed notes
+    :param notes_list: list of tuples representing note information (including onset time, offset time,
+    note frequency in hz, note name as a string, and list of frequencies included in interval)
+    :param sample_rate: sample rate of audio file
+    :param file_name: string representing name of output wav file
+    """
 
-    wave_file.setnchannels(1)  # mono
-    wave_file.setframerate(sample_rate)
-    wave_file.setsampwidth(2)
+    audio_end_time = int(notes_list[-1][1] * sample_rate) + 1
+    frames = np.array([0 for i in range(audio_end_time + 1)])
 
     for note in notes_list:
         start_time, end_time, note_freq = note[0], note[1], note[2]
 
-        frame_start = librosa.time_to_frames(start_time, sr=sample_rate)
-        frame_end = librosa.time_to_frames(end_time, sr=sample_rate)
+        frames[int(start_time * sample_rate): int(end_time * sample_rate)] = note_freq
 
-        for f in range(frame_start, frame_end + 1):
-            data = struct.pack('<h', int(note_freq))
-            wave_file.writeframesraw(data)
-
-    wave_file.close()
+    wavio.write(f'regenerated_wav/{file_name}', frames, sample_rate, sampwidth=3)
 
 
-if __name__ == "__main__":
-    y, sr = librosa.load("audio_files/birthday.wav")
+y, sr = librosa.load("audio_files/birthday.wav")
+# y, sr = librosa.load("audio_files/twinkle.wav")
 
-    # Identify fundamental frequency
-    f0, voiced_flag, voiced_probs = librosa.pyin(y, fmin=librosa.note_to_hz('C2'), fmax=librosa.note_to_hz('C7'), sr=sr)
-    times = librosa.times_like(f0)
-    f0s, f0_times = process_fundamental_freqs(f0, times)
-    plot_fundamental_freqs(y, f0)
+# Identify fundamental frequency
+f0, voiced_flag, voiced_probs = librosa.pyin(y, fmin=librosa.note_to_hz('C2'), fmax=librosa.note_to_hz('C7'), sr=sr)
+times = librosa.times_like(f0)
+f0s, f0_times = process_fundamental_freqs(f0, times)
+# plot_fundamental_freqs(y, f0)
 
-    # Rhythm detection - onset.onset_detect
-    onsets = librosa.onset.onset_detect(y=y, sr=sr, units='time')
-    # plot_beats(y, onsets)
+# Rhythm detection - onset.onset_detect
+onsets = librosa.onset.onset_detect(y=y, sr=sr, units='time')
+# plot_beats(y, onsets)
 
-    # Rhythm detection - beat.beat_track
-    tempo, beats = librosa.beat.beat_track(y=y, sr=sr, units='time')
-    # plot_beats(y, beats)
+# Rhythm detection - beat.beat_track
+tempo, beats = librosa.beat.beat_track(y=y, sr=sr, units='time')
+# plot_beats(y, beats)
 
-    # librosa.beat.beat_track misses last onset, get that information from onsets and combine the two
-    final_beats = combine_onset_times(onsets, beats)
-    plot_beats(y, final_beats)
+# librosa.beat.beat_track misses last onset, get that information from onsets and combine the two
+final_beats = combine_onset_times(onsets, beats)
+# plot_beats(y, final_beats)
 
-    notes_df, notes_info = segment_notes(final_beats, f0s, f0_times)
+notes_df, notes_info = segment_notes(final_beats, f0s, f0_times, 'birthday_notes.js')
 
-    # notes_df.to_csv('twinkle.csv')
-    notes_df.to_csv('birthday.csv')
+# notes_df.to_csv('twinkle.csv')
+notes_df.to_csv('birthday.csv')
 
-    generate_wav(notes_info, sr, 'birthday_regenerated.wav')
+# generate_wav(notes_info, sr, 'twinkle_regenerated.wav')
